2f546380f03e1b65f89891c1026ef743923454c5
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-06-03 16:55:26 +0200

    Add tests : approve + transferFrom

--- tp-tests-erc20/test/erc20.test.js
+++ tp-tests-erc20/test/erc20.test.js
@@ -9,6 +9,7 @@ contract("MyToken", accounts => {
   const _initialSupply = new BN(10000);
   const _owner = accounts[0];
   const _recipient = accounts[1];
+  const _spender = accounts[2];
   const _decimal = new BN(18);
 
   let MyTokenInstance;
@@ -45,6 +46,47 @@ contract("MyToken", accounts => {
     let balanceOwnerAfterTransfer = await MyTokenInstance.balanceOf(_owner);
     let balanceRecipientAfterTransfer = await MyTokenInstance.balanceOf(_recipient)
 
+    expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
+    expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
+  });
+
+  // --- test approve ---
+  it("check if approval done", async () => {
+    let amount = new BN(10);
+    let AllowanceBeforeApproval = await MyTokenInstance.allowance(_owner, _spender);
+    expect(AllowanceBeforeApproval).to.be.bignumber.equal(new BN(0));
+ 
+    await MyTokenInstance.approve(_spender, amount);
+    
+    let AllowanceAfterApproval = await MyTokenInstance.allowance(_owner, _spender);
+    expect(AllowanceAfterApproval).to.be.bignumber.equal(amount);
+  });
+
+  // --- test transferFrom ---
+  // test la maj allowance après transfer : _approve(owner, spender, currentAllowance - amount);
+  it("check if transferFrom done checking allowance", async () => { 
+    let amount = new BN(10);
+    await MyTokenInstance.approve(_spender, amount);
+    await MyTokenInstance.transferFrom(_owner, _spender, new BN(5), { from: _spender});
+    expect(await MyTokenInstance.allowance(_owner, _spender)).to.be.bignumber.equal(new BN(5));
+  });
+
+  it("check if transferFrom done checking balance", async () => {
+    let amount = new BN(100);
+    
+    await MyTokenInstance.approve(_recipient, amount);
+ 
+    // les balances avant
+    let balanceOwnerBeforeTransfer = await MyTokenInstance.balanceOf(_owner);
+    let balanceRecipientBeforeTransfer = await MyTokenInstance.balanceOf(_recipient)
+    expect(balanceOwnerBeforeTransfer).to.be.bignumber.equal(_initialSupply);
+    expect(balanceRecipientBeforeTransfer).to.be.bignumber.equal(new BN(0));
+ 
+    await MyTokenInstance.transferFrom(_owner, _recipient, amount, { from: _recipient})
+ 
+    // les balances après
+    let balanceOwnerAfterTransfer = await MyTokenInstance.balanceOf(_owner);
+    let balanceRecipientAfterTransfer = await MyTokenInstance.balanceOf(_recipient)
     expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
     expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
   });
