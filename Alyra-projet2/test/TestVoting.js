1a37843fb158d7c061792c2cb288b6e9ce8f6fd8
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-06-04 23:15:53 +0200

    Delete import unused

--- Alyra-projet2/test/TestVoting.js
+++ Alyra-projet2/test/TestVoting.js
@@ -1,6 +1,6 @@
 const Voting = artifacts.require("./Voting.sol");
 const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
-const { expect, should } = require('chai');
+const { expect } = require('chai');
 
 contract('Voting', accounts => {
     const _owner = accounts[0];
